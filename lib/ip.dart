b6c2c885a8c90bf2424f0c61ac98abb918edca47
Dylan Kavundama <dylancorporation.cd@gmail.com>
2025-07-15 08:33:03 +0200

    add

--- lib/ip.dart
+++ lib/ip.dart
@@ -4,7 +4,7 @@
 // }
 
 // ignore: non_constant_identifier_names
-String Adress_IP = "https://easykivu.com/phonexa";
+// String Adress_IP = "https://easykivu.com/phonexa";
 
 // String Adress_IP = "http://192.168.43.46/API_VENTE";
 
